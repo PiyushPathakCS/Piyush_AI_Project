f96885ba30da5b52198fba6826797b6dd81af443
Mayank-badaulia <201b152@juetguna.in>
2022-05-26 12:12:36 +0530

    Perfection Slope Added

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -173,7 +173,8 @@ public class MainActivity extends AppCompatActivity {
     public String getInterpretation(int [][]dataFrame,double slope){
        //provide interpretation based on your slope analysis
         // Your code here
-
+        if(slope==0 && dataFrame[0][1]==3)
+            interpretor="You aciehved Perfection";
         if(slope<0.5){
            interpretor=  "slow learner";
         }
