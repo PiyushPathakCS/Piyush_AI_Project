f5b5abc4a72af2f8551e74fd1ee5883bf8eb7d75
PiyushPathakJhansi <Piyush@gmail.com>
2022-05-26 12:22:48 +0530

    Good Learner Elaborated

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -182,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
            interpretor=  "You learn but you are a slow learner";
         }
         if (slope>0.5){
-            interpretor= "good learner";
+            interpretor= "You are a good learner";
         }
         return interpretor;
     }
